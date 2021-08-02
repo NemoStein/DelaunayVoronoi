3cf9a64d44f0e66e5e890a02ce534cbe557d75ed
NemoStein <contato@nemostein.com.br>
2021-08-02 09:11:01 -0300

    Refactored Cell to extract calculations

--- src/Cell.js
+++ src/Cell.js
@@ -13,50 +13,19 @@ export class Cell {
     this.bc = bc
     this.ca = ca
 
+    this.a = ab.a
+    this.b = (this.a === bc.a ? bc.b : bc.a)
+    this.c = (this.b === ca.a ? ca.b : ca.a)
+
+    this.circumcenter = calculateCircumcenter(this.a, this.b, this.c)
+    this.circumradius = calculateCircumradius(this.a, this.b, this.c)
+    this.centroid = calculateCentroid(this.a, this.b, this.c)
+
     this.virtual = ab.virtual || bc.virtual || ca.virtual
-
-    const a = this.a = ab.a
-    const b = this.b = (a === bc.a ? bc.b : bc.a)
-    const c = this.c = (b === ca.a ? ca.b : ca.a)
-
-    const d = 2 * (
-      a.x * (b.y - c.y) +
-      b.x * (c.y - a.y) +
-      c.x * (a.y - b.y)
-    )
-
-    this.circumcenter = new Point(
-      (
-        (a.x * a.x + a.y * a.y) * (b.y - c.y) +
-        (b.x * b.x + b.y * b.y) * (c.y - a.y) +
-        (c.x * c.x + c.y * c.y) * (a.y - b.y)
-      ) / d,
-      -(
-        (a.x * a.x + a.y * a.y) * (b.x - c.x) +
-        (b.x * b.x + b.y * b.y) * (c.x - a.x) +
-        (c.x * c.x + c.y * c.y) * (a.x - b.x)
-      ) / d
-    )
-
-    const dab = Point.distance(a, b)
-    const dbc = Point.distance(b, c)
-    const dca = Point.distance(c, a)
-
-    this.circumradius = (dab * dbc * dca) / Math.sqrt(
-      (dab + dbc + dca) *
-      (dbc + dca - dab) *
-      (dca + dab - dbc) *
-      (dab + dbc - dca)
-    )
-
-    this.centroid = new Point(
-      1 / 3 * (a.x + b.x + c.x),
-      1 / 3 * (a.y + b.y + c.y)
-    )
   }
 
   /**
-   * @param {Site | Edge} siteOrEdge
+   * @param {Site|Edge} siteOrEdge
    *
    * @returns {boolean} If this Cell have this Site/Edge as one of its vertices/sides
    */
@@ -71,4 +40,60 @@ export class Cell {
 
     throw new Error('Expected "siteOrEdge" to be of type Site or Edge')
   }
+}
+
+/**
+ * @param {Point} a
+ * @param {Point} b
+ * @param {Point} c
+ */
+const calculateCentroid = (a, b, c) => {
+  return new Point(
+    1 / 3 * (a.x + b.x + c.x),
+    1 / 3 * (a.y + b.y + c.y)
+  )
+}
+
+/**
+ * @param {Point} a
+ * @param {Point} b
+ * @param {Point} c
+ */
+const calculateCircumradius = (a, b, c) => {
+  const dab = Point.distance(a, b)
+  const dbc = Point.distance(b, c)
+  const dca = Point.distance(c, a)
+
+  return (dab * dbc * dca) / Math.sqrt(
+    (dab + dbc + dca) *
+    (dbc + dca - dab) *
+    (dca + dab - dbc) *
+    (dab + dbc - dca)
+  )
+}
+
+/**
+ * @param {Point} a
+ * @param {Point} b
+ * @param {Point} c
+ */
+const calculateCircumcenter = (a, b, c) => {
+  const d = 2 * (
+    a.x * (b.y - c.y) +
+    b.x * (c.y - a.y) +
+    c.x * (a.y - b.y)
+  )
+
+  return new Point(
+    (
+      (a.x * a.x + a.y * a.y) * (b.y - c.y) +
+      (b.x * b.x + b.y * b.y) * (c.y - a.y) +
+      (c.x * c.x + c.y * c.y) * (a.y - b.y)
+    ) / d,
+    -(
+      (a.x * a.x + a.y * a.y) * (b.x - c.x) +
+      (b.x * b.x + b.y * b.y) * (c.x - a.x) +
+      (c.x * c.x + c.y * c.y) * (a.x - b.x)
+    ) / d
+  )
 }
