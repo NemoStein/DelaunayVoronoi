b580143ff5a0daa194007f1f204a8ac9ad7f7586
NemoStein <contato@nemostein.com.br>
2021-09-11 17:10:14 -0300

    Updated Renderer

--- view/GraphRenderer.js
+++ view/GraphRenderer.js
@@ -1,4 +1,12 @@
 /** @typedef {import('@sourbit/geom').Triangle} Triangle */
+/** @typedef {import('../src/Delaunay.js').Delaunay} Delaunay */
+
+/**
+ * @callback Renderer
+ * @param {Delaunay} graph
+ * @param {CanvasRenderingContext2D} context
+ * @returns {void}
+ */
 
 export class GraphRenderer {
   /**
@@ -15,102 +23,151 @@ export class GraphRenderer {
   }
 
   /**
-   * @param {import('../src/Delaunay.js').Delaunay} graph
+   * @param {Delaunay} graph
    * @param {import('./app.js').DrawOptions} options
    */
   draw (graph, options) {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
 
-    for (const cell of graph.cells) {
-      if (options.drawCells) {
-        this.context.beginPath()
+    if (options.drawCells) {
+      this.render(graph, renderCells)
+    }
 
-        const alpha = (cell.circumcenter.x + cell.circumcenter.y) / 100 % 1
-        this.context.fillStyle = `rgba(0, 0, 0, ${alpha * 0.4 + 0.1})`
-        this.context.moveTo(cell.a.x, cell.a.y)
-        this.context.lineTo(cell.b.x, cell.b.y)
-        this.context.lineTo(cell.c.x, cell.c.y)
-        this.context.fill()
-      }
+    if (options.drawCircumcircles) {
+      this.render(graph, renderCircumcircles)
+    }
 
-      if (options.drawCircumcircles) {
-        this.context.beginPath()
-        this.context.lineWidth = 1
-        this.context.strokeStyle = 'rgba(0, 0, 0, 0.2)'
-
-        this.context.arc(cell.circumcenter.x, cell.circumcenter.y, cell.circumradius, 0, Math.PI * 2)
-        this.context.stroke()
-      }
-
-      if (options.drawCircumcenter) {
-        this.context.beginPath()
-        this.context.fillStyle = 'blue'
-        this.context.fillRect(cell.circumcenter.x - 3, cell.circumcenter.y - 3, 6, 6)
-        this.context.fill()
-      }
+    if (options.drawCircumcenter) {
+      this.render(graph, renderCircumcenters)
     }
 
     if (options.drawEdges) {
-      for (const edge of graph.edges) {
-        this.context.beginPath()
-        this.context.lineWidth = 1.5
-        this.context.strokeStyle = 'rgba(0, 0, 0, 0.5)'
-
-        this.context.moveTo(edge.a.x, edge.a.y)
-        this.context.lineTo(edge.b.x, edge.b.y)
-        this.context.stroke()
-      }
+      this.render(graph, renderEdges)
     }
 
     if (options.drawSites) {
-      for (const site of graph.sites) {
-        this.context.beginPath()
-        this.context.fillStyle = 'red'
-        this.context.fillRect(site.x - 3, site.y - 3, 6, 6)
-        this.context.fill()
-      }
+      this.render(graph, renderSites)
     }
 
     if (options.drawVoronoi) {
-      /** @type {Map<Triangle, Triangle[]>} */
-      const neighbourhood = new Map()
-      for (const cell of graph.cells) {
-        const neighbours = []
-        for (const neighbour of graph.cells) {
-          if (cell !== neighbour) {
-            if (neighbour.has(cell.ab) || neighbour.has(cell.bc) || neighbour.has(cell.ca)) {
-              neighbours.push(neighbour)
-            }
-          }
-        }
-        neighbourhood.set(cell, neighbours)
-      }
-
-      this.context.beginPath()
-      this.context.lineWidth = 1
-      this.context.strokeStyle = 'rgba(0, 255, 0, 0.5)'
-      for (const [cell, neighbours] of neighbourhood) {
-        for (const neighbor of neighbours) {
-          this.context.moveTo(cell.circumcenter.x, cell.circumcenter.y)
-          this.context.lineTo(neighbor.circumcenter.x, neighbor.circumcenter.y)
-          this.context.stroke()
-        }
-      }
+      this.render(graph, renderVoronoi)
     }
 
     if (options.drawMST) {
-      const msp = graph.generateMSP()
-
-      this.context.beginPath()
-      this.context.lineWidth = 4
-      this.context.strokeStyle = 'black'
-
-      for (const segment of msp) {
-        this.context.moveTo(segment.a.x, segment.a.y)
-        this.context.lineTo(segment.b.x, segment.b.y)
-      }
-
-      this.context.stroke()
+      this.render(graph, renderMST)
     }
   }
+
+  /**
+   * @param {Delaunay} graph
+   * @param {Renderer} renderer
+   */
+  render (graph, renderer) {
+    this.context.save()
+    renderer(graph, this.context)
+    this.context.restore()
+  }
+}
+
+/** @type {Renderer} */
+const renderCells = (graph, context) => {
+  for (const cell of graph.cells) {
+    context.beginPath()
+
+    const alpha = (cell.circumcenter.x + cell.circumcenter.y) / 100 % 1
+    context.fillStyle = `rgba(0, 0, 0, ${alpha * 0.4 + 0.1})`
+    context.moveTo(cell.a.x, cell.a.y)
+    context.lineTo(cell.b.x, cell.b.y)
+    context.lineTo(cell.c.x, cell.c.y)
+    context.fill()
+  }
+}
+
+/** @type {Renderer} */
+const renderCircumcircles = (graph, context) => {
+  for (const cell of graph.cells) {
+    context.beginPath()
+    context.lineWidth = 1
+    context.strokeStyle = 'rgba(0, 0, 0, 0.2)'
+
+    context.arc(cell.circumcenter.x, cell.circumcenter.y, cell.circumradius, 0, Math.PI * 2)
+    context.stroke()
+  }
+}
+
+/** @type {Renderer} */
+const renderCircumcenters = (graph, context) => {
+  for (const cell of graph.cells) {
+    context.beginPath()
+    context.fillStyle = 'blue'
+    context.fillRect(cell.circumcenter.x - 3, cell.circumcenter.y - 3, 6, 6)
+    context.fill()
+  }
+}
+
+/** @type {Renderer} */
+const renderEdges = (graph, context) => {
+  for (const edge of graph.edges) {
+    context.lineWidth = 1.5
+    context.strokeStyle = 'rgba(0, 0, 0, 0.5)'
+
+    context.beginPath()
+    context.moveTo(edge.a.x, edge.a.y)
+    context.lineTo(edge.b.x, edge.b.y)
+    context.stroke()
+  }
+}
+
+/** @type {Renderer} */
+const renderSites = (graph, context) => {
+  for (const site of graph.sites) {
+    context.beginPath()
+    context.fillStyle = 'red'
+    context.fillRect(site.x - 3, site.y - 3, 6, 6)
+    context.fill()
+  }
+}
+
+/** @type {Renderer} */
+const renderVoronoi = (graph, context) => {
+  /** @type {Map<Triangle, Triangle[]>} */
+  const neighbourhood = new Map()
+  for (const cell of graph.cells) {
+    const neighbours = []
+    for (const neighbour of graph.cells) {
+      if (cell !== neighbour) {
+        if (neighbour.has(cell.ab) || neighbour.has(cell.bc) || neighbour.has(cell.ca)) {
+          neighbours.push(neighbour)
+        }
+      }
+    }
+    neighbourhood.set(cell, neighbours)
+  }
+
+  context.lineWidth = 1
+  context.strokeStyle = 'rgba(0, 255, 0, 0.5)'
+  for (const [cell, neighbours] of neighbourhood) {
+    for (const neighbor of neighbours) {
+      context.beginPath()
+      context.moveTo(cell.circumcenter.x, cell.circumcenter.y)
+      context.lineTo(neighbor.circumcenter.x, neighbor.circumcenter.y)
+      context.stroke()
+    }
+  }
+}
+
+/** @type {Renderer} */
+const renderMST = (graph, context) => {
+  const mst = graph.generateMST()
+
+  context.beginPath()
+  context.lineWidth = 4
+  context.strokeStyle = 'black'
+
+  for (const segment of mst) {
+    context.moveTo(segment.a.x, segment.a.y)
+    context.lineTo(segment.b.x, segment.b.y)
+  }
+
+  context.stroke()
 }
